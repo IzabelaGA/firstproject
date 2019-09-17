539fa710d8f0ddd3f895c09918ea313f7b457cd6
IzabelaGA <izabela.sosnowska23@gmail.com>
2019-09-17 22:51:03 +0200

    Add correct process.php, contact form works, but isn't really ready yet

--- process.php
+++ process.php
@@ -1,14 +1,13 @@
 <?php
-$to ='izabela.sosnowska23@gmail.com';
-$name=$_POST['name'];
-$email=$_POST['email'];
-
-$subject='Nowy mail ze strony na GitHub od ' . $name . ' (' . $email .')';
-$message=$_POST['message'];
-$headers='From: ' . $name . ' (' . $email .')';
-$headers .="Content-Type: text/html; charset=utf-8\r\n"
+$to = 'izabela.sosnowska23@gmail.com';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject= 'Nowy mail od ' . $name . ' (' . $email .')';
+$message = $_POST['message'];
+// $headers = 'From: ' . $name'' . ' (' . $email .')';
+$headers = 'From: ' . $_POST['email'] . "\r\n" .
+$headers .= 'Content-type: text/html; charset=utf-8\r\n';
 
 mail($to, $subject, $message, $headers);
+echo 'Wiadomosć wysłana';
 ?>
-
-
